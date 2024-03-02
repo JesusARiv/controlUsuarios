e394a92e718253f14a9dbf3769172b7aec1fe1f5
vanevazque <144852611+vanevazque@users.noreply.github.com>
2024-03-01 21:40:40 -0600

    Add files via upload
    se subio un archivo

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-expand-md navbar-blue navbar-light shadow p-1"
+    style="background-color: #162332; min-height: 4rem;" id="barra">
+    <div class="container">
+        <h5 class="text-white">Sistema Escolar &nbsp<i class="fa-solid fa-graduation-cap"></i></h5>
+    </div>
+</nav>
+@push('scripts')
+@endpush
